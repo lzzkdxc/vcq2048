3256c676eea51529f0524f7fef4d301d822112c2
lzzkdxc <lzzkdxc@github.com>
2018-02-10 22:21:33 +0800

    .

---
+++ vcq2048.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<time.h>
+#include <stdlib.h>
+#include<windows.h>
+#include <conio.h>
+int a[9]={0},put2;
+void start()
+{
+	srand(time(NULL));
+}
+
+void out()
+{
+	
+}
+
+void in()
+{
+	input=getch;
+	while(a[put2]!=0)
+		put2=rand()%9;
+	a[put2]=2;
+	if(input==w)
+		
+}
+int main()
+{
+	
+} 
